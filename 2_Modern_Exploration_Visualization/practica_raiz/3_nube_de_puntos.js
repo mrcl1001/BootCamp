e0dd087423091d481cbb3af9de976f33ce9d3ec8
marcelogl <33746038+marcelogl@users.noreply.github.com>
2020-07-21 00:24:44 +0200

    ejercicios en clase

--- 2_Modern_Exploration_Visualization/practica_raiz/3_nube_de_puntos.js
+++ 2_Modern_Exploration_Visualization/practica_raiz/3_nube_de_puntos.js
@@ -98,10 +98,108 @@ var data = [{
     ]
     //Formateando datos
 var f = d3.format(".1f"); //nos devuelve un string
-data.forEach((d) => {})
+data.forEach((d) => {
+    d.area = +f(d.area * 0.09290304);
+    d.precio = +f(d.precio * 0.87);
+})
 console.log(data)
 
+var width = 800;
+var height = 600;
 
+var svg = d3.select("div")
+    .append("svg")
+    .attr("width", width + 100)
+    .attr("height", height + 100)
+    .append("g")
+    .attr("transform", "translate(50,0)");
+
+
+var tooltip = d3.select("div")
+    .append("div")
+    .attr("class", "tooltip")
+    .style("position", "absolute")
+    .style()
+
+/*console.log(d3.min(data, (d) => {
+return d.area
+}))*/
+
+var xmax = d3.max(data, (d) => {
+    return d.area
+})
+var xmin = d3.min(data, (d) => {
+    return d.area
+})
+
+var ymax = d3.max(data, (d) => {
+    return d.precio
+})
+var ymin = d3.min(data, (d) => {
+    return d.precio
+})
+var scalex = d3.scaleLinear()
+    .domain([xmin, xmax])
+    .range([0, width]);
+
+var scaley = d3.scaleLinear()
+    .domain([ymin, ymax])
+    .range([height / 2, 0]);
+
+var Xaxis = d3.axisBottom(scalex);
+var Yaxis = d3.axisLeft(scaley);
+
+var circle = svg.selectAll("circle")
+    .data(data)
+    .enter()
+    .append("circle")
+    .attr("cx", (d) => {
+        return scalex(d.area)
+    })
+    .attr("cy", (d) => {
+        return scaley(d.precio)
+    })
+    .attr("r", 5)
+    .attr("fill", "green");
+
+var texto = svg.selectAll("text")
+    .data(data)
+    .enter()
+    .append("text")
+    .attr("id", function(d, i) {
+        return "text_" + i
+    })
+    .attr("x", (d) => {
+        return scalex(d.area)
+    })
+    .attr("y", (d) => {
+        return scaley(d.precio) - 15
+    })
+    .text(function(d) {
+        return "Area: " + d.area + " Precio: " + d.precio
+    })
+    .attr("visibility", "hidden");
+
+circle.on("mouseover", function(d, i) {
+        d3.select(this)
+            .attr("visibility", "visible")
+            .attr("fill", "orange");
+        d3.select("#text_" + i).attr("visibility", "visible");
+    })
+    .on("mouseout", function(d, i) {
+        d3.select(this)
+            .attr("fill", "green");
+        d3.select("#text_" + i).attr("visibility", "hidden");
+    })
+
+svg.append("g").attr("transform", "translate(0," + height / 2 + ")").call(Xaxis);
+svg.append("g").call(Yaxis);
+
+
+
+
+
+//console.log(scalex(74.3))
 
 //Para el tooltip
 /*.style("left", (d3.event.pageX + 20) + "px")
